2988dc830d0cc463e2a98f387b600a3bf18866fa
cabaassulieman <cabaassulieman@users.noreply.github.com>
2021-11-18 22:31:41 +0300

    added some item

--- add.js
+++ add.js
@@ -4,6 +4,9 @@
 function addTodo() {
 const inputElement = document.querySelector('input');
 const value = inputElement.value;
+if(value=== ''){
+       return;
+}
 
 const todocontant = document.createElement('div');
 todocontant.className = 'flex justify-between items-center';
@@ -14,6 +17,7 @@ todoTitle.textContent = value;
 const doneButton = document.createElement('button');
 doneButton.className = 'bg-green-500 rounded p-2 shadow-lg';
 doneButton.textContent = 'Done';
+doneButton.onclick = markAsDone;
 
 todocontant.appendChild(todoTitle);
 todocontant.appendChild(doneButton);
@@ -24,4 +28,14 @@ const Alltodocontianer = document.querySelector('#todoContainer');
 Alltodocontianer.appendChild(todocontant);
 inputElement.value = '';
 
+}
+
+function markAsDone(event){
+  const clickedButton = event.target;
+  clickedButton.classList.add('hidden');
+  
+  const todoitem = clickedButton.previousElementSibling;
+
+  todoitem.classList.add('line-through');
+  
 }
